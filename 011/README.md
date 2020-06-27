790c46977e9624e1a3068416d3c1cefc36c7ac3a
CRTejaswi <aeronfinium@gmail.com>
2020-06-27 15:38:38 +0530

    Day 11 - JSON Autocomplete

---
+++ 011/README.md
@@ -0,0 +1,7 @@
+    Copyright (c) 2020-
+    Author: Chaitanya Tejaswi (github.com/CRTejaswi)    License: GPL v3.0+
+
+## Day 11 - JSON Auto-Complete <sup>[[source]](/011) [[video]](https://www.youtube.com/watch?v=1iysNUrI3lw)</sup>
+An auto-complete application with data parsed from a JSON file.
+
+![Auto-Complete](resources/app.gif)
---
+++ 011/src/js/main.js
@@ -0,0 +1,29 @@
+/* JS: Implements the Auto-Complete utility with data imported from a JSON file. */
+
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+const searchLatex = async searchText => {
+    const response = await fetch('https://crtejaswi.github.io/API/latexEquations.json');
+    const entries = await response.json();
+
+    let matches = entries.filter(entry => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return entry.title.match(regex);
+    });
+    if (searchText.length === 0){
+        matches = []; matchList.innerHTML='';
+    }
+    outputHtml(matches);
+}
+const outputHtml = matches => {
+    if (matches.length > 0){
+        const html = matches.map(
+            match => `
+                <h4>${match.title}: ${match.equation}
+                </h4>
+        `).join('');
+        matchList.innerHTML = html;
+    }
+}
+
+search.addEventListener('input', () => searchLatex(search.value));
